72b44780765382c69cbf22d35450b956f522780b
beesman93 <tomas@chvojka.dev>
2024-12-19 08:39:42 +0100

    Day 19 make it even ugglier

--- Solutions/Day19.cs
+++ Solutions/Day19.cs
@@ -12,14 +12,10 @@ namespace AOC_2024
         {
             towels = [.. _input[0].Split(", ")];
             texts = _input[2.._input.Length];
-            cache = new(new Dictionary<string, long>() { { "", 1 } });
+            cache = []; cache[""] = 1;
         }
-        public override ValueTask<string> Solve_1() => new($"{texts.AsParallel().Count(t => GetToEnd(t) > 0)}");
-        public override ValueTask<string> Solve_2() => new($"{texts.Sum(GetToEnd)}");
-        long GetToEnd(string r)
-        {
-            if (cache.TryGetValue(r, out long val)) return val;
-            return cache[r] = towels.Where(t => t[0] == r[0] && t.Length <= r.Length && r[..t.Length] == t).Sum(towel => GetToEnd(r[towel.Length..]));
-        }
+        public override ValueTask<string> Solve_1() => new($"{texts.AsParallel().Count(t => G(t) > 0)}");
+        public override ValueTask<string> Solve_2() => new($"{texts.Sum(G)}");
+        long G(string r) => cache.GetOrAdd(r,_=>towels.Where(t=>t[0]==r[0]&&t.Length<=r.Length&&r[..t.Length]== t).Sum(t=>G(r[t.Length..])));
     }
 }
